8731a295eb1ce6a26a27aad697adf585fb003c8c
2dos <2dossrl@gmail.com>
2022-05-05 20:06:33 +0000

    Automated release: 2022-05-05, 20:06:33

--- randomizer/Settings.py
+++ randomizer/Settings.py
@@ -16,7 +16,7 @@ class Settings:
 	def __init__(A,form_data):
 		'Init all the settings using the form data to set the flags.\n\n        Args:\n            form_data (dict): Post data from the html form.\n        ';A.__hash=A.__get_hash();A.public_hash=A.__get_hash();A.algorithm='forward';A.generate_main();A.generate_progression();A.generate_misc()
 		for (B,C) in form_data.items():setattr(A,B,C)
-		A.seed_id=str(A.seed);A.seed=str(A.seed)+A.__hash;A.set_seed();A.update_progression_totals();A.EntryGBs=[A.blocker_0,A.blocker_1,A.blocker_2,A.blocker_3,A.blocker_4,A.blocker_5,A.blocker_6,A.blocker_7];A.BossBananas=[A.troff_0,A.troff_1,A.troff_2,A.troff_3,A.troff_4,A.troff_5,A.troff_6];A.seed_hash=[random.randint(0,9)for A in range(5)];A.krool_keys_required=[];A.blocker_max=50;A.troff_max=270;A.troff_min=round(A.troff_max/3);A.training_barrels='startwith';A.shuffle_items=_D;A.progressive_upgrades=_B;A.prices=VanillaPrices.copy();A.resolve_settings()
+		A.seed_id=str(A.seed);A.seed=str(A.seed)+A.__hash;A.set_seed();A.seed_hash=[random.randint(0,9)for A in range(5)];A.krool_keys_required=[];A.blocker_max=50;A.troff_max=270;A.troff_min=round(A.troff_max/3);A.training_barrels='startwith';A.shuffle_items=_D;A.progressive_upgrades=_B;A.prices=VanillaPrices.copy();A.resolve_settings()
 	def update_progression_totals(A):
 		"Update the troff and blocker totals if we're randomly setting them.";A.troff_weight_0=0.7;A.troff_weight_1=0.8;A.troff_weight_2=0.9;A.troff_weight_3=1.0;A.troff_weight_4=1.1;A.troff_weight_5=1.2;A.troff_weight_6=1.3
 		if A.level_randomization==_F or A.level_randomization==_G:A.troff_weight_0=1;A.troff_weight_1=1;A.troff_weight_2=1;A.troff_weight_3=1;A.troff_weight_4=1;A.troff_weight_5=1;A.troff_weight_6=1
@@ -26,6 +26,7 @@ class Settings:
 			if A.shuffle_loading_zones==_E:random.shuffle(B)
 			else:B.sort()
 			A.blocker_0=B[0];A.blocker_1=B[1];A.blocker_2=B[2];A.blocker_3=B[3];A.blocker_4=B[4];A.blocker_5=B[5];A.blocker_6=B[6];A.blocker_7=B[7]
+		A.EntryGBs=[A.blocker_0,A.blocker_1,A.blocker_2,A.blocker_3,A.blocker_4,A.blocker_5,A.blocker_6,A.blocker_7];A.BossBananas=[A.troff_0,A.troff_1,A.troff_2,A.troff_3,A.troff_4,A.troff_5,A.troff_6]
 	def generate_main(A):'Set Default items on main page.';A.seed=_A;A.download_patch_file=_A;A.bonus_barrel_rando=_A;A.loading_zone_coupled=_A;A.shop_location_rando=_A;A.random_prices=_A;A.boss_location_rando=_A;A.boss_kong_rando=_A;A.kasplat_rando=_A
 	def set_seed(A):'Forcibly re-set the random seed to the seed set in the config.';random.seed(A.seed)
 	def generate_progression(A):'Set default items on progression page.';A.blocker_0=_A;A.blocker_1=_A;A.blocker_2=_A;A.blocker_3=_A;A.blocker_4=_A;A.blocker_5=_A;A.blocker_6=_A;A.blocker_7=_A;A.troff_0=_A;A.troff_1=_A;A.troff_2=_A;A.troff_3=_A;A.troff_4=_A;A.troff_5=_A;A.troff_6=_A;A.troff_min=_A;A.troff_max=_A
@@ -33,7 +34,7 @@ class Settings:
 	def resolve_settings(A):
 		'Resolve settings which are not directly set through the UI.';L='levels';K='random';J='random_helm';F='vanilla';G=GetKongs()
 		if A.random_prices!=F:A.prices=RandomizePrices(A.random_prices)
-		A.krool_donkey=_B;A.krool_diddy=_B;A.krool_lanky=_B;A.krool_tiny=_B;A.krool_chunky=_C;C=[A for A in G if A!=Kongs.chunky]
+		A.update_progression_totals();A.krool_donkey=_B;A.krool_diddy=_B;A.krool_lanky=_B;A.krool_tiny=_B;A.krool_chunky=_C;C=[A for A in G if A!=Kongs.chunky]
 		if A.krool_phase_order_rando:random.shuffle(C)
 		if A.krool_phase_count<5:C=random.sample(C,A.krool_phase_count-1)
 		B=[]
