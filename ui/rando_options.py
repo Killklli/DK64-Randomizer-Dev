f9fc3f14aaafa6d38fdd113dafb34ea82e3596a5
Killklli <tvatter@hotmail.com>
2022-06-04 21:24:18 +0000

    Automated release: 2022-06-04, 21:24:17

--- ui/rando_options.py
+++ ui/rando_options.py
@@ -1,55 +1,56 @@
 'Options for the main rando tab.'
-_b='gnawty_barrels'
-_a='krool_random'
-_Z='starting_kongs_count'
-_Y='random_music'
-_X='random_colors'
-_W='level_order'
-_V='shop_location_rando'
-_U='kong_rando'
-_T='randomize_cb_required_amounts'
-_S='randomize_blocker_required_amounts'
-_R='focusout'
-_Q='troff_'
-_P='blocker_'
-_O='enable_tag_anywhere'
+_c='gnawty_barrels'
+_b='krool_random'
+_a='starting_kongs_count'
+_Z='random_music'
+_Y='random_colors'
+_X='level_order'
+_W='shop_location_rando'
+_V='kong_rando'
+_U='randomize_cb_required_amounts'
+_T='randomize_blocker_required_amounts'
+_S='focusout'
+_R='troff_'
+_Q='blocker_'
+_P='enable_tag_anywhere'
+_O='change'
 _N='unlock_all_moves'
 _M='level_randomization'
-_L='change'
-_K='presets'
-_J='name'
-_I='keydown'
-_H='input'
-_G='disable_tag_barrels'
-_F='troff_text'
-_E='blocker_text'
-_D='click'
-_C=True
-_B=False
+_L='presets'
+_K='keydown'
+_J='input'
+_I='disable_tag_barrels'
+_H='name'
+_G='troff_text'
+_F='blocker_text'
+_E='click'
+_D=None
+_C=False
+_B=True
 _A='disabled'
 import random,js
 from js import document
 from ui.bindings import bind
 def randomseed(evt):'Randomly generate a seed ID.';document.getElementById('seed').value=str(random.randint(100000,999999))
-@bind(_H,_P,8)
-@bind(_H,_Q,8)
-@bind(_H,_E)
-@bind(_H,_F)
+@bind(_J,_Q,8)
+@bind(_J,_R,8)
+@bind(_J,_F)
+@bind(_J,_G)
 def on_input(event):
 	'Limits inputs from input boxes on keypress.\n\n    Args:\n        event (domevent): The DOMEvent data.\n\n    Returns:\n        bool: False if we need to stop the event.\n    ';A=event
-	if A.target.id==_E:return
-	elif A.target.id==_F:return
+	if A.target.id==_F:return
+	elif A.target.id==_G:return
 	elif'troff'in A.target.id:min_max(A,0,500)
 	elif'blocker'in A.target.id:min_max(A,0,200)
-@bind(_R,_E)
+@bind(_S,_F)
 def max_randomized_blocker(event):
-	'Validate blocker input on loss of focus.';A=js.document.getElementById(_E)
+	'Validate blocker input on loss of focus.';A=js.document.getElementById(_F)
 	if not A.value:A.value=50
 	elif 0<=int(A.value)<8:A.value=8
 	elif int(A.value)>200:A.value=200
-@bind(_R,_F)
+@bind(_S,_G)
 def max_randomized_troff(event):
-	'Validate troff input on loss of focus.';A=js.document.getElementById(_F)
+	'Validate troff input on loss of focus.';A=js.document.getElementById(_G)
 	if not A.value:A.value=300
 	elif int(A.value)>500:A.value=500
 def min_max(event,min,max):
@@ -59,46 +60,27 @@ def min_max(event,min,max):
 		elif int(A.target.value)<=min:A.preventDefault();document.getElementById(A.target.id).value=min
 		else:document.getElementById(A.target.id).value=str(A.target.value)
 	except Exception:A.preventDefault();document.getElementById(A.target.id).value=min
-@bind(_I,_P,8)
-@bind(_I,_Q,8)
-@bind(_I,_E)
-@bind(_I,_F)
+@bind(_K,_Q,8)
+@bind(_K,_R,8)
+@bind(_K,_F)
+@bind(_K,_G)
 def key_down(event):
 	'Check if a key is a proper number, deletion, navigation, Copy/Cut/Paste.\n\n    Args:\n        event (DomEvent): Event from the DOM.\n    ';A=event;B=['Backspace','Delete','ArrowLeft','ArrowRight','Control_L','Control_R','x','v','c']
 	if not A.key.isdigit()and A.key not in B:A.preventDefault()
 	else:0
 def set_preset_options():
-	'Set the Blocker presets on the page.';C=document.getElementById(_K);D=[]
-	for E in C.children:D.append(E.value)
-	for A in js.progression_presets:
-		if A.get(_J)not in D:B=document.createElement('option');B.value=A.get(_J);B.innerHTML=A.get(_J);B.title=A.get('description');C.appendChild(B)
-	js.jq('#presets').val('Suggested');toggle_counts_boxes(None);toggle_b_locker_boxes(None);js.load_cookies()
-@bind(_L,_K)
-def preset_select_changed(event):
-	'Trigger a change of the form via the JSON templates.';U='in-selection';T='label-in-selection';S='label-is-selection';R='data-slider-value';I='checked';G='%';V=document.getElementById(_K);B=None
-	for J in js.progression_presets:
-		if J.get(_J)==V.value:B=J
-	for A in B:
-		try:
-			if type(B[A])is bool:
-				if B[A]is _B:document.getElementsByName(A)[0].removeAttribute(I)
-				else:document.getElementsByName(A)[0].setAttribute(I,I)
-			elif document.getElementsByName(A)[0].hasAttribute(R):
-				K=document.getElementsByName(A)[0];H=document.getElementById(A);W=H.parentElement;E=W.getElementsByClassName('slider')[0];L=int(K.getAttribute('data-slider-min'));X=int(K.getAttribute('data-slider-max'));Y=B[A]-L;Z=X-L;F=Y/Z*100;M=E.getElementsByClassName('slider-track')[0];M.getElementsByClassName('slider-selection')[0].style.width=str(F)+G;M.getElementsByClassName('slider-track-high')[0].style.width=str(100-F)+G;N=E.getElementsByClassName('tooltip-main')[0];N.style.left=str(F)+G;N.getElementsByClassName('tooltip-inner')[0].innerText=B[A];a=E.getElementsByClassName('slider-tick-label-container')[0];b=E.getElementsByClassName('slider-tick-container')[0];D=a.getElementsByClassName('slider-tick-label');O=b.getElementsByClassName('slider-tick')
-				for C in range(len(D)):
-					P=int(D[C].innerText)
-					if P==B[A]:D[C].classList.add(S)
-					else:D[C].classList.remove(S)
-					if P<=B[A]:D[C].classList.add(T);O[C].classList.add(U)
-					else:D[C].classList.remove(T);O[C].classList.remove(U)
-				Q=E.getElementsByClassName('min-slider-handle')[0];Q.setAttribute('aria-valuenow',str(B[A]));Q.style.left=str(F)+G;H.setAttribute('data-value',str(B[A]));H.setAttribute('value',str(B[A]));document.getElementsByName(A)[0].setAttribute(R,B[A])
-			else:js.jq(f"#{A}").val(B[A])
-		except Exception as c:pass
-@bind(_D,_S)
+	'Set the Blocker presets on the page.';E='-- Select a Preset --';C=document.getElementById(_L);D=[]
+	for F in C.children:D.append(F.value)
+	for B in js.progression_presets:
+		if B.get(_H)not in D:
+			A=document.createElement('option');A.value=B.get(_H);A.innerHTML=B.get(_H);A.title=B.get('description');C.appendChild(A)
+			if B.get(_H)==E:A.disabled=_B;A.hidden=_B
+	js.jq('#presets').val(E);toggle_counts_boxes(_D);toggle_b_locker_boxes(_D);js.load_cookies()
+@bind(_E,_T)
 def toggle_b_locker_boxes(event):
-	'Toggle the textboxes for BLockers.';A=_C
-	if js.document.getElementById(_S).checked:A=_B
-	B=js.document.getElementById(_E)
+	'Toggle the textboxes for BLockers.';A=_B
+	if js.document.getElementById(_T).checked:A=_C
+	B=js.document.getElementById(_F)
 	if A:B.setAttribute(_A,_A)
 	else:B.removeAttribute(_A)
 	for D in range(0,10):
@@ -107,11 +89,11 @@ def toggle_b_locker_boxes(event):
 			if A:C.removeAttribute(_A)
 			else:C.setAttribute(_A,_A)
 		except AttributeError:pass
-@bind(_D,_T)
+@bind(_E,_U)
 def toggle_counts_boxes(event):
-	'Toggle the textboxes for Troff.';A=_C
-	if js.document.getElementById(_T).checked:A=_B
-	B=js.document.getElementById(_F)
+	'Toggle the textboxes for Troff.';A=_B
+	if js.document.getElementById(_U).checked:A=_C
+	B=js.document.getElementById(_G)
 	if A:B.setAttribute(_A,_A)
 	else:B.removeAttribute(_A)
 	for D in range(0,10):
@@ -120,73 +102,90 @@ def toggle_counts_boxes(event):
 			if A:C.removeAttribute(_A)
 			else:C.setAttribute(_A,_A)
 		except AttributeError:pass
-@bind(_L,_M)
+@bind(_O,_M)
 def update_boss_required(evt):
-	'Disable certain page flags depending on checkboxes.';E=document.getElementById(_M);B=document.getElementById('boss_location_rando');C=document.getElementById('boss_kong_rando');D=document.getElementById(_U);A=document.getElementById(_V);F=document.getElementById(_N)
-	if E.value==_W:B.setAttribute(_A,_A);B.checked=_C;C.setAttribute(_A,_A);C.checked=_C;D.setAttribute(_A,_A);D.checked=_C;A.setAttribute(_A,_A);A.checked=_C
+	'Disable certain page flags depending on checkboxes.';E=document.getElementById(_M);B=document.getElementById('boss_location_rando');C=document.getElementById('boss_kong_rando');D=document.getElementById(_V);A=document.getElementById(_W);F=document.getElementById(_N)
+	if E.value==_X:B.setAttribute(_A,_A);B.checked=_B;C.setAttribute(_A,_A);C.checked=_B;D.setAttribute(_A,_A);D.checked=_B;A.setAttribute(_A,_A);A.checked=_B
 	else:
 		try:C.removeAttribute(_A);B.removeAttribute(_A);D.removeAttribute(_A);A.removeAttribute(_A)
 		except Exception:pass
 	if F.checked:
-		try:A.setAttribute(_A,_A);A.checked=_B
+		try:A.setAttribute(_A,_A);A.checked=_C
 		except Exception:pass
-@bind(_D,_X)
+@bind(_E,_Y)
 def disable_colors(evt):
-	'Disable color options when Randomize All is selected.';A=_B
-	if js.document.getElementById(_X).checked:A=_C
+	'Disable color options when Randomize All is selected.';A=_C
+	if js.document.getElementById(_Y).checked:A=_B
 	for C in ['dk','diddy','tiny','lanky','chunky']:
 		B=js.document.getElementById(f"{C}_colors")
 		try:
 			if A:B.setAttribute(_A,_A)
 			else:B.removeAttribute(_A)
 		except AttributeError:pass
-@bind(_D,_O)
+@bind(_E,_P)
 def disable_tag_spawn(evt):
-	"Disable 'Disable Tag Spawn' option when 'Tag Anywhere' is off.";A=_B
-	if js.document.getElementById(_O).checked is _B:A=_C
-	if A:js.document.getElementById(_G).setAttribute(_A,_A);js.document.getElementById(_G).checked=_B
-	else:js.document.getElementById(_G).removeAttribute(_A)
-@bind(_D,_G)
+	"Disable 'Disable Tag Spawn' option when 'Tag Anywhere' is off.";A=_C
+	if js.document.getElementById(_P).checked is _C:A=_B
+	if A:js.document.getElementById(_I).setAttribute(_A,_A);js.document.getElementById(_I).checked=_C
+	else:js.document.getElementById(_I).removeAttribute(_A)
+@bind(_E,_I)
 def enable_tag_anywhere(evt):
 	"Enable 'Tag Anywhere' if 'Disable Tag Spawn' option is on."
-	if js.document.getElementById(_G).checked:js.document.getElementById(_O).checked=_C
-@bind(_D,_Y)
+	if js.document.getElementById(_I).checked:js.document.getElementById(_P).checked=_B
+@bind(_E,_Z)
 def disable_music(evt):
-	'Disable music options when Randomize All is selected.';A=_B
-	if js.document.getElementById(_Y).checked:A=_C
+	'Disable music options when Randomize All is selected.';A=_C
+	if js.document.getElementById(_Z).checked:A=_B
 	for C in ['bgm','fanfares','events']:
 		B=js.document.getElementById(f"music_{C}")
 		try:
 			if A:B.setAttribute(_A,_A)
 			else:B.removeAttribute(_A)
 		except AttributeError:pass
-@bind(_L,_Z)
+@bind(_O,_a)
 def enable_kong_rando(evt):
-	'Enable Kong Rando if less than 5 starting kongs.';print('Lanky');A=js.document.getElementById(_U)
-	if js.document.getElementById(_Z).value=='5':A.checked=_B;A.setAttribute(_A,_A)
+	'Enable Kong Rando if less than 5 starting kongs.';print('Lanky');A=js.document.getElementById(_V)
+	if js.document.getElementById(_a).value=='5':A.checked=_C;A.setAttribute(_A,_A)
 	else:A.removeAttribute(_A)
-@bind(_D,_a)
+@bind(_E,_b)
 def disable_krool_phases(evt):
-	'Disable music options when Randomize All is selected.';A=_B;B=js.document.getElementById('krool_phase_count')
-	if js.document.getElementById(_a).checked:A=_C
+	'Disable music options when Randomize All is selected.';A=_C;B=js.document.getElementById('krool_phase_count')
+	if js.document.getElementById(_b).checked:A=_B
 	try:
 		if A:B.setAttribute(_A,_A)
 		else:B.removeAttribute(_A)
 	except AttributeError:pass
-@bind(_D,_N)
+@bind(_E,_N)
 def disable_shuffle_shop(evt):
-	'Disable Shuffle Shop Move Location when All Moves are Unlocked.';C=_B;A=js.document.getElementById(_V);B=js.document.getElementById('random_prices');D=js.document.getElementById(_N)
-	if D.checked:C=_C
+	'Disable Shuffle Shop Move Location when All Moves are Unlocked.';C=_C;A=js.document.getElementById(_W);B=js.document.getElementById('random_prices');D=js.document.getElementById(_N)
+	if D.checked:C=_B
 	try:
-		if C:A.setAttribute(_A,_A);A.checked=_B;B.setAttribute(_A,_A)
-		elif js.document.getElementById(_M).value!=_W:A.removeAttribute(_A);B.removeAttribute(_A)
-		else:B.removeAttribute(_A);A.checked=_C
+		if C:A.setAttribute(_A,_A);A.checked=_C;B.setAttribute(_A,_A)
+		elif js.document.getElementById(_M).value!=_X:A.removeAttribute(_A);B.removeAttribute(_A)
+		else:B.removeAttribute(_A);A.checked=_B
 	except AttributeError:pass
-@bind(_D,_b)
+@bind(_E,_c)
 def disable_barrel_rando(evt):
-	'Disable Bonus Barrel Rando when Oops All Beaver Bother is selected.';B=_B;A=js.document.getElementById('bonus_barrel_rando')
-	if js.document.getElementById(_b).checked:B=_C
+	'Disable Bonus Barrel Rando when Oops All Beaver Bother is selected.';B=_C;A=js.document.getElementById('bonus_barrel_rando')
+	if js.document.getElementById(_c).checked:B=_B
 	try:
-		if B:A.setAttribute(_A,_A);A.checked=_B
+		if B:A.setAttribute(_A,_A);A.checked=_C
 		else:A.removeAttribute(_A)
 	except AttributeError:pass
+@bind(_O,_L)
+def preset_select_changed(event):
+	'Trigger a change of the form via the JSON templates.';D=document.getElementById(_L);B=_D
+	for C in js.progression_presets:
+		if C.get(_H)==D.value:B=C
+	for A in B:
+		try:
+			if type(B[A])is bool:
+				if B[A]is _C:js.jq(f"#{A}").checked=_C;js.document.getElementsByName(A)[0].checked=_C
+				else:js.jq(f"#{A}").checked=_B;js.document.getElementsByName(A)[0].checked=_B
+				js.jq(f"#{A}").removeAttr(_A)
+			else:
+				if js.document.getElementsByName(A)[0].hasAttribute('data-slider-value'):js.jq(f"#{A}").slider('setValue',B[A]);js.jq(f"#{A}").slider('enable');js.jq(f"#{A}").parent().find('.slider-disabled').removeClass('slider-disabled')
+				else:js.jq(f"#{A}").val(B[A])
+				js.jq(f"#{A}").removeAttr(_A)
+		except Exception as E:pass
+	toggle_counts_boxes(_D);toggle_b_locker_boxes(_D);update_boss_required(_D);disable_colors(_D);disable_music(_D);disable_shuffle_shop(_D);max_randomized_blocker(_D);max_randomized_troff(_D);disable_barrel_rando(_D)
