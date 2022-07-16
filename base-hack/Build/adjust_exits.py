8d5ed69a7f541588c77d0247db87f86fb22f0833
theballaam96 <theballaam96@gmail.com>
2022-07-16 04:42:16 +0000

    Automated release: 2022-07-16, 04:42:16

--- base-hack/Build/adjust_exits.py
+++ base-hack/Build/adjust_exits.py
@@ -8,7 +8,8 @@ _A='exit_index'
 from typing import BinaryIO
 pointer_table_address=1055824
 pointer_table_index=23
-exit_adjustments=[{_E:48,_F:[{_A:3,_B:3429,_C:462,_D:4494},{_A:6,_B:4153,_C:163,_D:3721},{_A:4,_B:3982,_C:115,_D:2026},{_A:5,_B:4550,_C:162,_D:3646}]},{_E:30,_F:[{_A:10,_B:1524,_C:1754,_D:3964},{_A:19,_B:3380,_C:1640,_D:120}]},{_E:112,_F:[{_A:1,_B:1515,_C:80,_D:2506}]},{_E:34,_F:[{_A:3,_B:3464,_C:1040,_D:1716}]},{_E:26,_F:[{_A:8,_B:814,_C:8,_D:1334}]},{_E:87,_F:[{_A:15,_B:1293,_C:472,_D:238},{_A:11,_B:1808,_C:1406,_D:1270}]}]
+new_caves_portal_coords=[120.997,50,1182.974]
+exit_adjustments=[{_E:48,_F:[{_A:3,_B:3429,_C:462,_D:4494},{_A:6,_B:4153,_C:163,_D:3721},{_A:4,_B:3982,_C:115,_D:2026},{_A:5,_B:4550,_C:162,_D:3646}]},{_E:30,_F:[{_A:10,_B:1524,_C:1754,_D:3964},{_A:19,_B:3380,_C:1640,_D:120}]},{_E:112,_F:[{_A:1,_B:1515,_C:80,_D:2506}]},{_E:34,_F:[{_A:3,_B:3464,_C:1040,_D:1716}]},{_E:26,_F:[{_A:8,_B:814,_C:8,_D:1334}]},{_E:87,_F:[{_A:15,_B:1293,_C:472,_D:238},{_A:11,_B:1808,_C:1406,_D:1270}]},{_E:72,_F:[{_A:11,_B:int(new_caves_portal_coords[0]-25),_C:int(new_caves_portal_coords[1]),_D:int(new_caves_portal_coords[2]-12)}]}]
 def adjustExits(fh):
 	'Write new exits.';B='big';A=fh;print('Adjusting Exits');A.seek(pointer_table_address+4*pointer_table_index);E=pointer_table_address+int.from_bytes(A.read(4),B)
 	for D in exit_adjustments:
--- base-hack/Build/convertSetup.py
+++ base-hack/Build/convertSetup.py
@@ -24,60 +24,77 @@ def float_to_hex(f):
 	return hex(struct.unpack('<I',struct.pack('<f',f))[0])
 base_stream=0
 def modify(file_name,map_index):
-	'Modify the file to be updated.\n\n    Args:\n        file_name (str): File name.\n        map_index (int): Map index.\n    ';l='base_byte_stream';f=file_name;e='rz';d='rx';W='scale';V='id';U='ry';T='z';S='y';R='x';M='type';I='stream';G=map_index;global base_stream
-	with open(f,'r+b')as m:
-		E=m.read();n=int.from_bytes(E[:4],_A);B=4;N=[];X=[];O=[];Y=[];g=[];Z=544;h=False
-		for A in range(n):
-			D=E[B:B+48];P=int.from_bytes(E[B+40:B+42],_A);K=int.from_bytes(E[B+42:B+44],_A)
-			if P==684 and G!=42:
-				base_stream=D;i=int.from_bytes(E[B+0:B+4],_A);a=int.from_bytes(E[B+4:B+8],_A);o=int_to_float(a)-30;a=int(float_to_hex(o),16);j=int.from_bytes(E[B+8:B+12],_A);y=int.from_bytes(E[B+24:B+28],_A);b=int.from_bytes(E[B+28:B+32],_A);z=int.from_bytes(E[B+32:B+36],_A);K=int.from_bytes(E[B+42:B+44],_A)
+	'Modify the file to be updated.\n\n    Args:\n        file_name (str): File name.\n        map_index (int): Map index.\n    ';s=False;i=file_name;h='base_byte_stream';d=b'';c='rz';b='rx';V='scale';U='id';T='ry';S='z';R='y';Q='x';M='type';K='stream';G=map_index;global base_stream
+	with open(i,'r+b')as t:
+		F=t.read();u=int.from_bytes(F[:4],_A);B=4;W=[];e=[];X=[];Y=[];j=[];N=544;k=s;l=s
+		for A in range(u):
+			D=F[B:B+48];Z=int.from_bytes(F[B+40:B+42],_A);H=int.from_bytes(F[B+42:B+44],_A)
+			if Z==684 and G!=42:
+				if G==72 and not l and H==38:
+					for a in range(2):m=50.167;Y.append({h:D,M:[684,683][a],Q:int(float_to_hex(120.997),16),R:[int(float_to_hex(m),16),int(float_to_hex(m-30),16)][a],S:int(float_to_hex(1182.974),16),b:0,T:int(float_to_hex(75.146),16),c:0,U:[368,N][a],V:[int(float_to_hex(1),16),int(float_to_hex(0.35),16)][a]})
+					N+=1;l=True
+				base_stream=D;n=int.from_bytes(F[B+0:B+4],_A);f=int.from_bytes(F[B+4:B+8],_A);v=int_to_float(f)-30;f=int(float_to_hex(v),16);o=int.from_bytes(F[B+8:B+12],_A);AB=int.from_bytes(F[B+24:B+28],_A);O=int.from_bytes(F[B+28:B+32],_A);AC=int.from_bytes(F[B+32:B+36],_A);H=int.from_bytes(F[B+42:B+44],_A)
 				if G==7:
-					if Z==544:i=int(float_to_hex(805.6618),16);j=int(float_to_hex(2226.797),16)
-				Y.append({l:D,M:683,R:i,S:a,T:j,d:0,U:b,e:0,V:Z,W:int(float_to_hex(0.35),16)});Z+=1
-			if G==34 and not h and K==6:h=True;Y.append({l:D,M:132,R:int(float_to_hex(2457.471),16),S:int(float_to_hex(1280),16),T:int(float_to_hex(3458.604),16),d:0,U:int(float_to_hex(166),16),e:0,V:256,W:int(float_to_hex(1.18),16)})
-			if G==7 and K==26 or G==176 and K==57:
-				P=206;F=b''
-				for A in range(41):F+=D[A].to_bytes(1,_A)
-				F+=P.to_bytes(1,_A)
-				for A in range(48-42):F+=D[A+42].to_bytes(1,_A)
-				D=F
-			if G==26 and K==36:
-				F=b'';Q=[0,0,0];c=[1455.853,6.5,522.716];Q[0]=int(float_to_hex(c[0]),16);Q[1]=int(float_to_hex(c[1]),16);Q[2]=int(float_to_hex(c[2]),16);b=int(float_to_hex(0),16)
-				for A in Q:F+=A.to_bytes(4,_A)
-				for A in range(28-12):F+=D[A+12].to_bytes(1,_A)
-				F+=b.to_bytes(4,_A)
-				for A in range(48-32):F+=D[A+32].to_bytes(1,_A)
-				D=F
-			L={I:D,M:P};N.append(L);B+=48
-		p=getMoveSignData(G,base_stream)
-		for q in p:g.append(q)
-		r=int.from_bytes(E[B:B+4],_A);B+=4
-		for A in range(r):D=E[B:B+36];L={I:D};X.append(L);B+=36
-		s=int.from_bytes(E[B:B+4],_A);B+=4
-		for A in range(s):
-			D=E[B:B+56];t=int.from_bytes(E[B+52:B+54],_A)
-			if G==26 and t==13:
-				k=[]
-				for H in range(56):k.append(D[H])
-				D=k.copy();u=1237.001;v=840.569;writedatatoarr(D,int(float_to_hex(u),16),4,0);writedatatoarr(D,int(float_to_hex(v),16),4,8)
-			L={I:D};O.append(L);B+=56
-		for A in g:
+					if N==544:n=int(float_to_hex(805.6618),16);o=int(float_to_hex(2226.797),16)
+				Y.append({h:D,M:683,Q:n,R:f,S:o,b:0,T:O,c:0,U:N,V:int(float_to_hex(0.35),16)});N+=1
+			if G==34 and not k and H==6:k=True;Y.append({h:D,M:132,Q:int(float_to_hex(2457.471),16),R:int(float_to_hex(1280),16),S:int(float_to_hex(3458.604),16),b:0,T:int(float_to_hex(166),16),c:0,U:256,V:int(float_to_hex(1.18),16)})
+			if G==7 and H==26 or G==176 and H==57:
+				Z=206;E=d
+				for A in range(41):E+=D[A].to_bytes(1,_A)
+				E+=Z.to_bytes(1,_A)
+				for A in range(48-42):E+=D[A+42].to_bytes(1,_A)
+				D=E
+			if G==26 and H==36:
+				E=d;I=[0,0,0];g=[1455.853,6.5,522.716];I[0]=int(float_to_hex(g[0]),16);I[1]=int(float_to_hex(g[1]),16);I[2]=int(float_to_hex(g[2]),16);O=int(float_to_hex(0),16)
+				for A in I:E+=A.to_bytes(4,_A)
+				for A in range(28-12):E+=D[A+12].to_bytes(1,_A)
+				E+=O.to_bytes(4,_A)
+				for A in range(48-32):E+=D[A+32].to_bytes(1,_A)
+				D=E
+			if G==26 and H>=103 and H<=118:
+				E=d;p=H-103;w=p%4;x=int(p/4);y=2606.114;z=1767.899;q=37.7;I=[0,0,0];I[0]=int(float_to_hex(y+q*x),16);I[1]=int(float_to_hex(1002),16);I[2]=int(float_to_hex(z+q*w),16)
+				for A in I:E+=A.to_bytes(4,_A)
+				for A in range(28-12):E+=D[A+12].to_bytes(1,_A)
+				E+=O.to_bytes(4,_A)
+				for A in range(48-32):E+=D[A+32].to_bytes(1,_A)
+				D=E;O=int(float_to_hex(180),16)
+			if G==72:
+				if H==87 or H==207:
+					E=d;A0=176.505;A1=1089.408;E+=int(float_to_hex(A0),16).to_bytes(4,_A)
+					for A in range(4):E+=D[A+4].to_bytes(1,_A)
+					E+=int(float_to_hex(A1),16).to_bytes(4,_A)
+					for A in range(48-12):E+=D[A+12].to_bytes(1,_A)
+					D=E
+			P={K:D,M:Z};W.append(P);B+=48
+		A2=getMoveSignData(G,base_stream)
+		for A3 in A2:j.append(A3)
+		A4=int.from_bytes(F[B:B+4],_A);B+=4
+		for A in range(A4):D=F[B:B+36];P={K:D};e.append(P);B+=36
+		A5=int.from_bytes(F[B:B+4],_A);B+=4
+		for A in range(A5):
+			D=F[B:B+56];A6=int.from_bytes(F[B+52:B+54],_A)
+			if G==26 and A6==13:
+				r=[]
+				for J in range(56):r.append(D[J])
+				D=r.copy();A7=1237.001;A8=840.569;writedatatoarr(D,int(float_to_hex(A7),16),4,0);writedatatoarr(D,int(float_to_hex(A8),16),4,8)
+			P={K:D};X.append(P);B+=56
+		for A in j:
 			C=[]
-			for H in range(56):C.append(0)
-			writedatatoarr(C,A[R],4,0);writedatatoarr(C,A[S],4,4);writedatatoarr(C,A[T],4,8);writedatatoarr(C,A[W],4,12);writedatatoarr(C,A[U],2,48);writedatatoarr(C,A[M],2,50);writedatatoarr(C,A[V],2,52);O.append({I:C})
+			for J in range(56):C.append(0)
+			writedatatoarr(C,A[Q],4,0);writedatatoarr(C,A[R],4,4);writedatatoarr(C,A[S],4,8);writedatatoarr(C,A[V],4,12);writedatatoarr(C,A[T],2,48);writedatatoarr(C,A[M],2,50);writedatatoarr(C,A[U],2,52);X.append({K:C})
 		for A in Y:
 			C=[]
-			for H in range(16):C.append(0)
-			w=[255,251,0,0,21,0,0,0,64,192,0,0,67,179,0,0]
-			for H in w:C.append(H)
-			for H in range(12):C.append(0)
-			x=[0,1,0,0]
-			for H in x:C.append(H)
-			C=writedatatoarr(C,A[M],2,40);C=writedatatoarr(C,A[V],2,42);C=writedatatoarr(C,A[W],4,12);C=writedatatoarr(C,A[R],4,0);C=writedatatoarr(C,A[S],4,4);C=writedatatoarr(C,A[T],4,8);C=writedatatoarr(C,A[d],4,24);C=writedatatoarr(C,A[U],4,28);C=writedatatoarr(C,A[e],4,32);N.append({I:C})
-		with open(f.replace(_B,_C),'wb')as J:
-			J.write(len(N).to_bytes(4,_A))
-			for A in N:J.write(bytearray(A[I]))
-			J.write(len(X).to_bytes(4,_A))
-			for A in X:J.write(bytearray(A[I]))
-			J.write(len(O).to_bytes(4,_A))
-			for A in O:J.write(bytearray(A[I]))
+			for J in range(16):C.append(0)
+			A9=[255,251,0,0,21,0,0,0,64,192,0,0,67,179,0,0]
+			for J in A9:C.append(J)
+			for J in range(12):C.append(0)
+			AA=[0,1,0,0]
+			for J in AA:C.append(J)
+			C=writedatatoarr(C,A[M],2,40);C=writedatatoarr(C,A[U],2,42);C=writedatatoarr(C,A[V],4,12);C=writedatatoarr(C,A[Q],4,0);C=writedatatoarr(C,A[R],4,4);C=writedatatoarr(C,A[S],4,8);C=writedatatoarr(C,A[b],4,24);C=writedatatoarr(C,A[T],4,28);C=writedatatoarr(C,A[c],4,32);W.append({K:C})
+		with open(i.replace(_B,_C),'wb')as L:
+			L.write(len(W).to_bytes(4,_A))
+			for A in W:L.write(bytearray(A[K]))
+			L.write(len(e).to_bytes(4,_A))
+			for A in e:L.write(bytearray(A[K]))
+			L.write(len(X).to_bytes(4,_A))
+			for A in X:L.write(bytearray(A[K]))
