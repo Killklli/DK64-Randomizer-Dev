e2c232aa49ebe73ac64ff5251acd36ab686c8979
theballaam96 <theballaam96@gmail.com>
2022-07-21 20:29:13 +0000

    Automated release: 2022-07-21, 20:29:13

--- base-hack/Build/convertSetup.py
+++ base-hack/Build/convertSetup.py
@@ -24,77 +24,90 @@ def float_to_hex(f):
 	return hex(struct.unpack('<I',struct.pack('<f',f))[0])
 base_stream=0
 def modify(file_name,map_index):
-	'Modify the file to be updated.\n\n    Args:\n        file_name (str): File name.\n        map_index (int): Map index.\n    ';s=False;i=file_name;h='base_byte_stream';d=b'';c='rz';b='rx';V='scale';U='id';T='ry';S='z';R='y';Q='x';M='type';K='stream';G=map_index;global base_stream
-	with open(i,'r+b')as t:
-		F=t.read();u=int.from_bytes(F[:4],_A);B=4;W=[];e=[];X=[];Y=[];j=[];N=544;k=s;l=s
-		for A in range(u):
-			D=F[B:B+48];Z=int.from_bytes(F[B+40:B+42],_A);H=int.from_bytes(F[B+42:B+44],_A)
-			if Z==684 and G!=42:
-				if G==72 and not l and H==38:
-					for a in range(2):m=50.167;Y.append({h:D,M:[684,683][a],Q:int(float_to_hex(120.997),16),R:[int(float_to_hex(m),16),int(float_to_hex(m-30),16)][a],S:int(float_to_hex(1182.974),16),b:0,T:int(float_to_hex(75.146),16),c:0,U:[368,N][a],V:[int(float_to_hex(1),16),int(float_to_hex(0.35),16)][a]})
-					N+=1;l=True
-				base_stream=D;n=int.from_bytes(F[B+0:B+4],_A);f=int.from_bytes(F[B+4:B+8],_A);v=int_to_float(f)-30;f=int(float_to_hex(v),16);o=int.from_bytes(F[B+8:B+12],_A);AC=int.from_bytes(F[B+24:B+28],_A);O=int.from_bytes(F[B+28:B+32],_A);AD=int.from_bytes(F[B+32:B+36],_A);H=int.from_bytes(F[B+42:B+44],_A)
+	'Modify the file to be updated.\n\n    Args:\n        file_name (str): File name.\n        map_index (int): Map index.\n    ';x=False;o=file_name;n='use_byte_stream';m=True;f=b'';e='rz';d='rx';T='scale';S='ry';R='base_byte_stream';P='id';O='z';N='y';M='x';L='type';K='stream';G=map_index;global base_stream
+	with open(o,'r+b')as y:
+		F=y.read();z=int.from_bytes(F[:4],_A);B=4;Y=[];g=[];Z=[];a=[];h=[];U=544;p=x;q=x
+		for A in range(z):
+			D=F[B:B+48];b=int.from_bytes(F[B+40:B+42],_A);H=int.from_bytes(F[B+42:B+44],_A)
+			if b==684 and G!=42:
+				if G==72 and not q and H==38:
+					for c in range(2):r=50.167;a.append({R:D,L:[684,683][c],M:int(float_to_hex(120.997),16),N:[int(float_to_hex(r),16),int(float_to_hex(r-30),16)][c],O:int(float_to_hex(1182.974),16),d:0,S:int(float_to_hex(75.146),16),e:0,P:[368,U][c],T:[int(float_to_hex(1),16),int(float_to_hex(0.35),16)][c]})
+					U+=1;q=m
+				base_stream=D;s=int.from_bytes(F[B+0:B+4],_A);i=int.from_bytes(F[B+4:B+8],_A);A0=int_to_float(i)-30;i=int(float_to_hex(A0),16);t=int.from_bytes(F[B+8:B+12],_A);AE=int.from_bytes(F[B+24:B+28],_A);V=int.from_bytes(F[B+28:B+32],_A);AF=int.from_bytes(F[B+32:B+36],_A);H=int.from_bytes(F[B+42:B+44],_A)
 				if G==7:
-					if N==544:n=int(float_to_hex(805.6618),16);o=int(float_to_hex(2226.797),16)
-				Y.append({h:D,M:683,Q:n,R:f,S:o,b:0,T:O,c:0,U:N,V:int(float_to_hex(0.35),16)});N+=1
-			if G==34 and not k and H==6:k=True;Y.append({h:D,M:132,Q:int(float_to_hex(2457.471),16),R:int(float_to_hex(1280),16),S:int(float_to_hex(3458.604),16),b:0,T:int(float_to_hex(166),16),c:0,U:256,V:int(float_to_hex(1.18),16)})
+					if U==544:s=int(float_to_hex(805.6618),16);t=int(float_to_hex(2226.797),16)
+				a.append({R:D,L:683,M:s,N:i,O:t,d:0,S:V,e:0,P:U,T:int(float_to_hex(0.35),16)});U+=1
+			if G==34 and not p and H==6:p=m;a.append({R:D,L:132,M:int(float_to_hex(2457.471),16),N:int(float_to_hex(1280),16),O:int(float_to_hex(3458.604),16),d:0,S:int(float_to_hex(166),16),e:0,P:256,T:int(float_to_hex(1.18),16)})
 			if G==7 and H==26 or G==176 and H==57:
-				Z=206;E=d
+				b=206;E=f
 				for A in range(41):E+=D[A].to_bytes(1,_A)
-				E+=Z.to_bytes(1,_A)
+				E+=b.to_bytes(1,_A)
 				for A in range(48-42):E+=D[A+42].to_bytes(1,_A)
 				D=E
 			if G==26 and H==36:
-				E=d;I=[0,0,0];g=[1455.853,6.5,522.716];I[0]=int(float_to_hex(g[0]),16);I[1]=int(float_to_hex(g[1]),16);I[2]=int(float_to_hex(g[2]),16);O=int(float_to_hex(0),16)
-				for A in I:E+=A.to_bytes(4,_A)
+				E=f;J=[0,0,0];j=[1455.853,6.5,522.716];J[0]=int(float_to_hex(j[0]),16);J[1]=int(float_to_hex(j[1]),16);J[2]=int(float_to_hex(j[2]),16);V=int(float_to_hex(0),16)
+				for A in J:E+=A.to_bytes(4,_A)
 				for A in range(28-12):E+=D[A+12].to_bytes(1,_A)
-				E+=O.to_bytes(4,_A)
+				E+=V.to_bytes(4,_A)
 				for A in range(48-32):E+=D[A+32].to_bytes(1,_A)
 				D=E
 			if G==26 and H>=103 and H<=118:
-				E=d;p=H-103;w=p%4;x=int(p/4);y=2606.114;z=1767.899;q=37.7;I=[0,0,0];I[0]=int(float_to_hex(y+q*x),16);I[1]=int(float_to_hex(1002),16);I[2]=int(float_to_hex(z+q*w),16)
-				for A in I:E+=A.to_bytes(4,_A)
+				E=f;u=H-103;A1=u%4;A2=int(u/4);A3=2606.114;A4=1767.899;v=37.7;J=[0,0,0];J[0]=int(float_to_hex(A3+v*A2),16);J[1]=int(float_to_hex(1002),16);J[2]=int(float_to_hex(A4+v*A1),16)
+				for A in J:E+=A.to_bytes(4,_A)
 				for A in range(28-12):E+=D[A+12].to_bytes(1,_A)
-				E+=O.to_bytes(4,_A)
+				E+=V.to_bytes(4,_A)
 				for A in range(48-32):E+=D[A+32].to_bytes(1,_A)
-				D=E;O=int(float_to_hex(180),16)
+				D=E;V=int(float_to_hex(180),16)
 			if G==72:
 				if H==87 or H==207:
-					E=d;A0=176.505;A1=1089.408;E+=int(float_to_hex(A0),16).to_bytes(4,_A)
+					E=f;A5=176.505;A6=1089.408;E+=int(float_to_hex(A5),16).to_bytes(4,_A)
 					for A in range(4):E+=D[A+4].to_bytes(1,_A)
-					E+=int(float_to_hex(A1),16).to_bytes(4,_A)
+					E+=int(float_to_hex(A6),16).to_bytes(4,_A)
 					for A in range(48-12):E+=D[A+12].to_bytes(1,_A)
 					D=E
-			P={K:D,M:Z};W.append(P);B+=48
-		A2=getMoveSignData(G,base_stream)
-		for A3 in A2:j.append(A3)
-		A4=int.from_bytes(F[B:B+4],_A);B+=4
-		for A in range(A4):D=F[B:B+36];P={K:D};e.append(P);B+=36
-		A5=int.from_bytes(F[B:B+4],_A);B+=4
-		for A in range(A5):
-			D=F[B:B+56];A6=int.from_bytes(F[B+52:B+54],_A)
-			if G==26 and A6==13:
-				r=[]
-				for J in range(56):r.append(D[J])
-				D=r.copy();A7=1237.001;A8=175;A9=840.569;writedatatoarr(D,int(float_to_hex(A7),16),4,0);writedatatoarr(D,int(float_to_hex(A8),16),4,4);writedatatoarr(D,int(float_to_hex(A9),16),4,8)
-			P={K:D};X.append(P);B+=56
-		for A in j:
+			W={K:D,L:b};Y.append(W);B+=48
+		A7=getMoveSignData(G,base_stream)
+		for A8 in A7:h.append(A8)
+		A9=int.from_bytes(F[B:B+4],_A);B+=4
+		for A in range(A9):D=F[B:B+36];W={K:D};g.append(W);B+=36
+		AA=int.from_bytes(F[B:B+4],_A);B+=4
+		for A in range(AA):
+			D=F[B:B+56];w=int.from_bytes(F[B+52:B+54],_A)
+			if G==26 and w==13:
+				X=[]
+				for I in range(56):X.append(D[I])
+				D=X.copy();k=1237.001;AB=175;l=840.569;writedatatoarr(D,int(float_to_hex(k),16),4,0);writedatatoarr(D,int(float_to_hex(AB),16),4,4);writedatatoarr(D,int(float_to_hex(l),16),4,8)
+			elif G==64 and w==4:
+				h.append({R:D,M:int(float_to_hex(517.993),16),N:int(float_to_hex(1070.614),16),O:int(float_to_hex(510.374),16),P:255,n:m});X=[]
+				for I in range(56):X.append(D[I])
+				D=X.copy();k=410.614;l=512.886;writedatatoarr(D,int(float_to_hex(k),16),4,0);writedatatoarr(D,int(float_to_hex(l),16),4,8)
+			W={K:D};Z.append(W);B+=56
+		for A in h:
 			C=[]
-			for J in range(56):C.append(0)
-			writedatatoarr(C,A[Q],4,0);writedatatoarr(C,A[R],4,4);writedatatoarr(C,A[S],4,8);writedatatoarr(C,A[V],4,12);writedatatoarr(C,A[T],2,48);writedatatoarr(C,A[M],2,50);writedatatoarr(C,A[U],2,52);X.append({K:C})
-		for A in Y:
+			for I in range(56):
+				if n in A and A[n]and R in A:C.append(A[R][I])
+				else:C.append(0)
+			if M in A:writedatatoarr(C,A[M],4,0)
+			if N in A:writedatatoarr(C,A[N],4,4)
+			if O in A:writedatatoarr(C,A[O],4,8)
+			if T in A:writedatatoarr(C,A[T],4,12)
+			if S in A:writedatatoarr(C,A[S],2,48)
+			if L in A:writedatatoarr(C,A[L],2,50)
+			if P in A:writedatatoarr(C,A[P],2,52)
+			Z.append({K:C})
+		for A in a:
 			C=[]
-			for J in range(16):C.append(0)
-			AA=[255,251,0,0,21,0,0,0,64,192,0,0,67,179,0,0]
-			for J in AA:C.append(J)
-			for J in range(12):C.append(0)
-			AB=[0,1,0,0]
-			for J in AB:C.append(J)
-			C=writedatatoarr(C,A[M],2,40);C=writedatatoarr(C,A[U],2,42);C=writedatatoarr(C,A[V],4,12);C=writedatatoarr(C,A[Q],4,0);C=writedatatoarr(C,A[R],4,4);C=writedatatoarr(C,A[S],4,8);C=writedatatoarr(C,A[b],4,24);C=writedatatoarr(C,A[T],4,28);C=writedatatoarr(C,A[c],4,32);W.append({K:C})
-		with open(i.replace(_B,_C),'wb')as L:
-			L.write(len(W).to_bytes(4,_A))
-			for A in W:L.write(bytearray(A[K]))
-			L.write(len(e).to_bytes(4,_A))
-			for A in e:L.write(bytearray(A[K]))
-			L.write(len(X).to_bytes(4,_A))
-			for A in X:L.write(bytearray(A[K]))
+			for I in range(16):C.append(0)
+			AC=[255,251,0,0,21,0,0,0,64,192,0,0,67,179,0,0]
+			for I in AC:C.append(I)
+			for I in range(12):C.append(0)
+			AD=[0,1,0,0]
+			for I in AD:C.append(I)
+			C=writedatatoarr(C,A[L],2,40);C=writedatatoarr(C,A[P],2,42);C=writedatatoarr(C,A[T],4,12);C=writedatatoarr(C,A[M],4,0);C=writedatatoarr(C,A[N],4,4);C=writedatatoarr(C,A[O],4,8);C=writedatatoarr(C,A[d],4,24);C=writedatatoarr(C,A[S],4,28);C=writedatatoarr(C,A[e],4,32);Y.append({K:C})
+		with open(o.replace(_B,_C),'wb')as Q:
+			Q.write(len(Y).to_bytes(4,_A))
+			for A in Y:Q.write(bytearray(A[K]))
+			Q.write(len(g).to_bytes(4,_A))
+			for A in g:Q.write(bytearray(A[K]))
+			Q.write(len(Z).to_bytes(4,_A))
+			for A in Z:Q.write(bytearray(A[K]))
--- base-hack/test_variables_apply.py
+++ base-hack/test_variables_apply.py
@@ -1,6 +1,10 @@
 'Set debugging vars to the build.'
+_E=' (offset '
+_D='- Writing '
+_C='test_zone'
+_B='rom/dk64-randomizer-base-dev.z64'
 _A='special_move_prices'
-set_variables={'level_order_rando_on':0,'level_order':[1,5,4,0,6,2,3],'troff_scoff_count':[100,200,300,400,410,420,430],'blocker_normal_count':[2,3,4,5,6,7,8,9],'key_flags':[74,138,168,236,292,317,26],'unlock_kongs':29,'unlock_moves':1,'fast_start_beginning':1,'camera_unlocked':1,'tag_anywhere':1,'fast_start_helm':0,'crown_door_open':0,'coin_door_open':0,'quality_of_life':1,'price_rando_on':1,'k_rool_order':[0,3,1,2,4],'damage_multiplier':0,'fps_on':1,'no_health_refill':0,'slam_prices':[4,5],'gun_prices':[1,2,3,4,5],'instrument_prices':[1,2,3,4,5],'gun_upgrade_prices':[1,2],'ammo_belt_prices':[1,2],'instrument_upgrade_prices':[1,2,3],'move_rando_on':1,'kut_out_kong_order':[0,0,0,0,0],'remove_blockers':127,'resolve_bonus':0,'disable_drops':1,'shop_indicator_on':1,'warp_to_isles_enabled':1,'lobbies_open_bitfield':255,'perma_lose_kongs':0,'jetpac_medal_requirement':1,'starting_kong':0,'free_target_llama':0,'free_source_llama':3,'keys_preturned':1,'short_bosses':1,'fast_warp':1,'activate_all_bananaports':1,'piano_game_order':[5,0,1,3,5,5,3],'dartboard_order':[0,1,2,3,4,5],'fast_gbs':1,'remove_high_requirements':1,'open_level_sections':1,'auto_keys':1,_A:[[1,2,3],[4,5,6],[7,8,9],[1,2,3],[4,5,6]]}
+set_variables={'level_order_rando_on':0,'level_order':[1,5,4,0,6,2,3],'troff_scoff_count':[100,200,300,400,410,420,430],'blocker_normal_count':[2,3,4,5,6,7,8,9],'key_flags':[74,138,168,236,292,317,26],'unlock_kongs':29,'unlock_moves':1,'fast_start_beginning':1,'camera_unlocked':1,'tag_anywhere':1,'fast_start_helm':0,'crown_door_open':0,'coin_door_open':0,'quality_of_life':1,'price_rando_on':1,'k_rool_order':[0,3,1,2,4],'damage_multiplier':0,'fps_on':1,'no_health_refill':0,'slam_prices':[4,5],'gun_prices':[1,2,3,4,5],'instrument_prices':[1,2,3,4,5],'gun_upgrade_prices':[1,2],'ammo_belt_prices':[1,2],'instrument_upgrade_prices':[1,2,3],'move_rando_on':1,'kut_out_kong_order':[0,0,0,0,0],'remove_blockers':127,'resolve_bonus':0,'disable_drops':1,'shop_indicator_on':1,'warp_to_isles_enabled':1,'lobbies_open_bitfield':255,'perma_lose_kongs':0,'jetpac_medal_requirement':1,'starting_kong':0,'free_target_llama':0,'free_source_llama':3,'keys_preturned':1,'short_bosses':1,'fast_warp':1,'activate_all_bananaports':1,'piano_game_order':[5,0,1,3,5,5,3],'dartboard_order':[0,1,2,3,4,5],'fast_gbs':1,'remove_high_requirements':1,'open_level_sections':1,'auto_keys':1,_C:[64,4],_A:[[1,2,3],[4,5,6],[7,8,9],[1,2,3],[4,5,6]]}
 def valtolst(val,size):
 	'Convert the values to a list.';B=size;C=[]
 	for D in range(B):C.append(0)
@@ -8,9 +12,15 @@ def valtolst(val,size):
 	for D in range(B):
 		if A!=0:C[B-D-1]=int(A%256);A=(A-A%256)/256
 	return C
+def readFromROM(offset,size):
+	'Read from ROM.'
+	with open(_B,'rb')as A:A.seek(offset);return int.from_bytes(A.read(size),'big')
+def writeToROMNoOffset(offset,value,size,name):
+	'Write to ROM without offset.';B=value;A=offset;print(_D+name+_E+hex(A)+') to '+str(B))
+	with open(_B,'r+b')as C:C.seek(A);C.write(bytearray(valtolst(B,size)))
 def writeToROM(offset,value,size,name):
-	'Write byte data to rom.';B=value;A=offset;print('- Writing '+name+' (offset '+hex(A)+') to '+str(B))
-	with open('rom/dk64-randomizer-base-dev.z64','r+b')as C:C.seek(33476640+A);C.write(bytearray(valtolst(B,size)))
+	'Write byte data to rom.';B=value;A=offset;print(_D+name+_E+hex(A)+') to '+str(B))
+	with open(_B,'r+b')as C:C.seek(33476640+A);C.write(bytearray(valtolst(B,size)))
 with open('include/variable_space_structs.h','r')as varspace:
 	varlines=varspace.readlines();struct_data=[]
 	for x in varlines:
@@ -37,6 +47,11 @@ with open('include/variable_space_structs.h','r')as varspace:
 					size=y[1];offset=y[0]
 					for kong in set_variables[_A]:
 						for lvl in kong:writeToROM(offset,lvl,size,x);offset+=size
+		elif x==_C:
+			ptr_table_offset=1055824;lz_table=ptr_table_offset+readFromROM(ptr_table_offset+18*4,4);isles_list=ptr_table_offset+readFromROM(lz_table+34*4,4);isles_list_end=ptr_table_offset+readFromROM(lz_table+34*4+4,4);isles_list_size=int((isles_list_end-isles_list)/56);isles_list+=2
+			for lz_index in range(isles_list_size):
+				lz_type=readFromROM(isles_list+56*lz_index+16,2);lz_map=readFromROM(isles_list+56*lz_index+18,2);lz_exit=readFromROM(isles_list+56*lz_index+20,2)
+				if lz_type==9 and lz_map==176 and lz_exit==0:writeToROMNoOffset(isles_list+56*lz_index+18,set_variables[x][0],2,'Isles -> TGrounds Zone Map');writeToROMNoOffset(isles_list+56*lz_index+20,set_variables[x][1],2,'Isles -> TGrounds Zone Exit')
 		else:
 			for y in struct_data2:
 				if x==y[2]:
