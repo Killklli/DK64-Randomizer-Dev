ac35fa093a3880532986737c9ca6b2982a976195
Cfox7 <cfox7.ace@gmail.com>
2022-08-13 06:07:13 +0000

    Automated release: 2022-08-13, 06:07:13

--- ui/settings_strings.py
+++ ui/settings_strings.py
@@ -4,7 +4,7 @@ from itertools import groupby
 import js
 def encrypt_settings_string(dict_data):
 	'Take a dictionary and return an encrypted string.\n\n    Args:\n        dict_data (dict): Posted JSON data from the form.\n\n    Returns:\n        str: Returns an encrypted string.\n    ';D=dict_data
-	for I in ['download_patch_file','seed','settings_string']:D.pop(I)
+	for I in ['download_patch_file','seed','settings_string','chunky_colors','chunky_custom_color','diddy_colors','diddy_custom_color','dk_colors','dk_custom_color','enguarde_colors','enguarde_custom_color','klaptrap_model','lanky_colors','lanky_custom_color','rambi_colors','rambi_custom_color','random_colors','random_music','music_bgm','music_events','music_fanfares','tiny_colors','tiny_custom_color']:D.pop(I)
 	B=collections.OrderedDict(sorted(D.items()));J=collections.OrderedDict(sorted(default_dict.items()));C=''
 	for A in B:
 		if J[A]==B[A]:C+=','
@@ -16,11 +16,11 @@ def encrypt_settings_string(dict_data):
 	for G in range(0,len(E),4):F.append(E[G:G+4])
 	def K(s_list):return[[len(list(B)),A]for(A,B)in groupby(s_list)]
 	H=''
-	for A in K(F):H+=';'+str(A[0])+':'+A[1]
+	for A in K(F):H+='|'+str(A[0])+':'+A[1]
 	return H
 def decrypt_setting_string(encrypted_string):
 	'Take an encrypted string and return a dictionary.\n\n    Args:\n        encrypted_string (str): Passed settings string.\n\n    Returns:\n        dict: Returns the decrypted set of data.\n    ';D=''
-	for A in encrypted_string.split(';'):
+	for A in encrypted_string.split('|'):
 		if A:
 			if':'in A:G,H=A.split(':');D+=H*int(G)
 			else:D+=A
