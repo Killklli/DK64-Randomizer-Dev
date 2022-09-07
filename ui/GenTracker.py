6b7b35e8cb17b26fd0c0df6387d4214e9e6e7c5c
Killklli <tvatter@hotmail.com>
2022-09-07 19:32:10 +0000

    Automated release: 2022-09-07, 19:32:10

--- ui/GenTracker.py
+++ ui/GenTracker.py
@@ -1,15 +1,22 @@
 'File which generates a basic HTML tracker table for your seed.'
 import json,js
 def generateTracker(spoilerJson):
-	'Use the tracker template and spoiler data to generate a basic tracker.';P='Shuffled Exits';O='</table>\n';N='</tr>\n';M='" class="spoiler">';L=')"/></td>\n';K='<tr>\n';J='<table>\n';I='Locations';F='</td>\n';E='">';C=js.getFile('./TrackerTemplate.html');D=json.loads(spoilerJson);G='<!--Start Generated Content-->';H=C.index(G)+len(G);A='\n';A+='<h4>Seed: '+D['Settings']['Seed']+'</h4>\n'
-	if I in D:
-		A+='<h3>Locations</h3>\n';A+=J;B=0
-		for (Q,R) in D[I].items():A+=K;A+='<td id="loc_'+str(B)+E+Q+F;A+='<td class="check-cell"><input id="itemCheck_'+str(B)+'" type="checkbox" onclick="showRow(\'item\', '+str(B)+L;A+='<td id="item_'+str(B)+M+R+F;A+=N;B+=1
-		A+=O;A+='<input type="hidden" id="locCount" value="'+str(B)+E
+	'Use the tracker template and spoiler data to generate a basic tracker.';V='Shuffled Level Order';U='Shuffled Exits';T='</table>\n';S='</tr>\n';R='" class="spoiler">';Q=')"/></td>\n';P='<tr>\n';O='<table>\n';N='Other';M='Shops';L='Kongs';I='</td>\n';H='">';D='Items';E=js.getFile('./TrackerTemplate.html');C=json.loads(spoilerJson);J='<!--Start Generated Content-->';K=E.index(J)+len(J);A='\n';A+='<h4>Seed: '+C['Settings']['Seed']+'</h4>\n'
+	if D in C:
+		F={}
+		if L in C[D]:F.update(C[D][L])
+		if M in C[D]:F.update(C[D][M])
+		if N in C[D]:F.update(C[D][N])
+		A+='<h3>Locations</h3>\n';A+=O;B=0
+		for (W,X) in F.items():A+=P;A+='<td id="loc_'+str(B)+H+W+I;A+='<td class="check-cell"><input id="itemCheck_'+str(B)+'" type="checkbox" onclick="showRow(\'item\', '+str(B)+Q;A+='<td id="item_'+str(B)+R+X+I;A+=S;B+=1
+		A+=T;A+='<input type="hidden" id="locCount" value="'+str(B)+H
 	else:A+='<input type="hidden" id="locCount" value="0">'
-	if P in D:
-		A+='<h3>Exits</h3>\n';A+=J;B=0
-		for (S,T) in D[P].items():A+=K;A+='<td id="front_'+str(B)+E+S+F;A+='<td class="check-cell"><input id="backCheck_'+str(B)+'" type="checkbox" onclick="showRow(\'back\', '+str(B)+L;A+='<td id="back_'+str(B)+M+T+F;A+=N;B+=1
-		A+=O;A+='<input type="hidden" id="exitCount" value="'+str(B)+E
+	G={}
+	if U in C:G.update(C[U])
+	if V in C:G.update(C[V])
+	if len(G)>0:
+		A+='<h3>Exits</h3>\n';A+=O;B=0
+		for (Y,Z) in G.items():A+=P;A+='<td id="front_'+str(B)+H+Y+I;A+='<td class="check-cell"><input id="backCheck_'+str(B)+'" type="checkbox" onclick="showRow(\'back\', '+str(B)+Q;A+='<td id="back_'+str(B)+R+Z+I;A+=S;B+=1
+		A+=T;A+='<input type="hidden" id="exitCount" value="'+str(B)+H
 	else:A+='<input type="hidden" id="exitCount" value="0">'
-	C=C[:H]+A+C[H:];return C
+	E=E[:K]+A+E[K:];return E
