b907ec743e0618433711df0a61add0b86ed7db41
theballaam96 <theballaam96@gmail.com>
2022-11-15 12:58:26 +0000

    Automated release: 2022-11-15, 12:58:26

--- randomizer/Patching/CrownPlacer.py
+++ randomizer/Patching/CrownPlacer.py
@@ -12,38 +12,38 @@ class CrownPlacementShortData:
 def randomize_crown_pads(spoiler):
 	'Place Crown Pads.';E=spoiler;D='big'
 	if E.settings.crown_placement_rando:
-		N=[];O=[Maps.JungleJapes,Maps.AztecTinyTemple,Maps.FranticFactory,Maps.GloomyGalleon,Maps.FungiForest,Maps.CavesRotatingCabin,Maps.CastleGreenhouse,Maps.IslesSnideRoom,Maps.FungiForestLobby,Maps.HideoutHelm];F=[];K=O.copy()
+		N=[];O=[Maps.JungleJapes,Maps.AztecTinyTemple,Maps.FranticFactory,Maps.GloomyGalleon,Maps.FungiForest,Maps.CavesRotatingCabin,Maps.CastleGreenhouse,Maps.IslesSnideRoom,Maps.FungiForestLobby,Maps.HideoutHelm];J=[];K=O.copy()
 		for L in E.crown_locations:
 			for A in E.crown_locations[L]:
 				B=CrownLocations[L][A];X=E.crown_locations[L][A];N.append(CrownPlacementShortData(B.map,B.coords,B.scale,X,B.is_vanilla))
-				if B.is_vanilla and B.map in F:F.append(B.map)
+				if B.is_vanilla:J.append(B.map)
 				if not B.is_vanilla:
 					if B.map not in K:K.append(B.map)
 		for C in K:
 			if C==Maps.CavesRotatingCabin:
-				if C not in F:Y=E.settings.rom_data;ROM().seek(Y+405);ROM().write(1)
+				if C not in J:Y=E.settings.rom_data;ROM().seek(Y+405);ROM().write(1)
 			else:
-				G=js.pointer_addresses[9]['entries'][C]['pointing_to'];ROM().seek(G);P=int.from_bytes(ROM().readBytes(4),D);H=[]
+				F=js.pointer_addresses[9]['entries'][C]['pointing_to'];ROM().seek(F);P=int.from_bytes(ROM().readBytes(4),D);G=[]
 				for Z in range(P):
-					Q=True;R=G+4+Z*48;ROM().seek(R+40);a=int.from_bytes(ROM().readBytes(2),D)
-					if C in O and C not in F and a==454:Q=False
+					Q=True;R=F+4+Z*48;ROM().seek(R+40);a=int.from_bytes(ROM().readBytes(2),D)
+					if C in O and C not in J and a==454:Q=False
 					if Q:
 						ROM().seek(R);S=[]
 						for T in range(int(48/4)):S.append(int.from_bytes(ROM().readBytes(4),D))
-						H.append(S)
+						G.append(S)
 				U=[]
 				for A in N:
 					if A.map==C and not A.vanilla:
-						I=getNextFreeID(C,U);U.append(I);H.append([int(float_to_hex(A.coords[0]),16),int(float_to_hex(A.coords[1]),16),int(float_to_hex(A.coords[2]),16),int(float_to_hex(A.scale),16),1795943986,2605527663,0,0,0,0,454<<16|I,1<<16])
-						if A.default==0:addNewScript(C,[I],ScriptTypes.CrownMain)
-						elif A.default==1:addNewScript(C,[I],ScriptTypes.CrownIsles2)
-				ROM().seek(G+4+P*48);V=int.from_bytes(ROM().readBytes(4),D);J=[V]
+						H=getNextFreeID(C,U);U.append(H);G.append([int(float_to_hex(A.coords[0]),16),int(float_to_hex(A.coords[1]),16),int(float_to_hex(A.coords[2]),16),int(float_to_hex(A.scale),16),1795943986,2605527663,0,0,0,0,454<<16|H,1<<16])
+						if A.default==0:addNewScript(C,[H],ScriptTypes.CrownMain)
+						elif A.default==1:addNewScript(C,[H],ScriptTypes.CrownIsles2)
+				ROM().seek(F+4+P*48);V=int.from_bytes(ROM().readBytes(4),D);I=[V]
 				for c in range(V):
-					for T in range(int(36/4)):J.append(int.from_bytes(ROM().readBytes(4),D))
-				W=int.from_bytes(ROM().readBytes(4),D);J.append(W)
+					for T in range(int(36/4)):I.append(int.from_bytes(ROM().readBytes(4),D))
+				W=int.from_bytes(ROM().readBytes(4),D);I.append(W)
 				for d in range(W):
-					for T in range(int(56/4)):J.append(int.from_bytes(ROM().readBytes(4),D))
-				ROM().seek(G);ROM().writeMultipleBytes(len(H),4)
-				for b in H:
+					for T in range(int(56/4)):I.append(int.from_bytes(ROM().readBytes(4),D))
+				ROM().seek(F);ROM().writeMultipleBytes(len(G),4)
+				for b in G:
 					for M in b:ROM().writeMultipleBytes(M,4)
-				for M in J:ROM().writeMultipleBytes(M,4)
+				for M in I:ROM().writeMultipleBytes(M,4)
